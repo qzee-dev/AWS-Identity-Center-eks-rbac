58fbcbd23cddecbb8012028a5b6fc583212c2ebf
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 16:09:37 +0100

    Add kubernetes_groups to EKS access entry

--- access-entry/devops.tf
+++ access-entry/devops.tf
@@ -1,8 +1,11 @@
 resource "aws_eks_access_entry" "devops" {
 
   cluster_name  = var.cluster_name
-
   principal_arn = var.devops_role_arn
+
+  kubernetes_groups = [
+    "devops-team"
+  ]
 
   type = "STANDARD"
 }
