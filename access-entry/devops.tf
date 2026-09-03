66aa04690c8de6f405cbfc5081e24a0bafd12132
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 15:08:48 +0100

    Update devops.tf

--- access-entry/devops.tf
+++ access-entry/devops.tf
@@ -6,3 +6,17 @@ resource "aws_eks_access_entry" "devops" {
 
   type = "STANDARD"
 }
+
+
+resource "aws_eks_access_policy_association" "devops" {
+
+  cluster_name = var.cluster_name
+
+  principal_arn = var.devops_role_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
