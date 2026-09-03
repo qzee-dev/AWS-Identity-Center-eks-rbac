f80926e2d3ea8a59325f2786976c2c66701b5a79
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 14:42:47 +0100

    Add AWS-related variables to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "aws_region" {
+  type = string
+}
 
+variable "cluster_name" {
+  type = string
+}
+
+variable "devops_role_arn" {
+  type = string
+}
+
+variable "developer_role_arn" {
+  type = string
+}
+
+variable "security_role_arn" {
+  type = string
+}
