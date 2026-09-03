315a46563d45e0cc3cb386847e59c792da143b83
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 16:16:48 +0100

    Add kubernetes_groups to EKS access entry
    Added kubernetes_groups to aws_eks_access_entry resource.

--- access-entry/security.tf
+++ access-entry/security.tf
@@ -1,12 +1,14 @@
 resource "aws_eks_access_entry" "security" {
 
   cluster_name  = var.cluster_name
-
   principal_arn = var.security_role_arn
+
+  kubernetes_groups = [
+    "security-team"
+  ]
 
   type = "STANDARD"
 }
-
 
 resource "aws_eks_access_policy_association" "security" {
 
