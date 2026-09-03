0ed5c38716fc1b2fbe4254c8e8205c7213e9abb7
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 15:09:51 +0100

    Add EKS access policy association for developer role

--- access-entry/developer.tf
+++ access-entry/developer.tf
@@ -6,3 +6,18 @@ resource "aws_eks_access_entry" "developer" {
 
   type = "STANDARD"
 }
+
+
+resource "aws_eks_access_policy_association" "developer" {
+
+  cluster_name = var.cluster_name
+
+  principal_arn = var.developer_role_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+``
