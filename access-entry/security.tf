7e02e54e2d71f56185d90bc3ce3e59118a9b5848
Qoseem Oluwaphemy <tech.Assistancebas@gmail.com>
2026-09-03 15:10:55 +0100

    Add EKS access policy association resource

--- access-entry/security.tf
+++ access-entry/security.tf
@@ -6,3 +6,17 @@ resource "aws_eks_access_entry" "security" {
 
   type = "STANDARD"
 }
+
+
+resource "aws_eks_access_policy_association" "security" {
+
+  cluster_name = var.cluster_name
+
+  principal_arn = var.security_role_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
